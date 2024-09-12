45046f27dcbdafc6ba52c8c96a00d24303947129
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2024-09-12 16:16:09 +0500

    Add grid toggle

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,57 @@
 // Total width of the grid.
-const GRIDSIZE = 600;
+const GRID_WIDTH = getComputedStyle(document.body).getPropertyValue("--grid-width");
+const ACCENT_COLOR = getComputedStyle(document.body).getPropertyValue("--accent-color");
+const INACTIVE_COLOR = getComputedStyle(document.body).getPropertyValue("--inactive-color");
 
 // Default grid of 16x16 dimensions.
 let rowsAndCols = 16;
 
-
 const SKETCH_AREA = document.querySelector("#sketch-area");
-const SLIDER_CONTAINER = document.querySelector("#container");
+const CONTAINER = document.querySelector(".container");
 const SLIDER = document.querySelector("#slider");
 const SLIDER_VALUE = document.querySelector("#slider-value");
 
-SLIDER_VALUE.textContent = `${SLIDER.value} x ${SLIDER.value} (Resolution)`;
-// Chained assignment.
-SKETCH_AREA.style.width = SKETCH_AREA.style.height = `${GRIDSIZE}px`;
+const GRID_TOGGLE = document.querySelector("#grid-toggle");
+
+let gridVisible = false; //
+
+function toggleGrid() {
+    gridVisible = gridVisible ? false : true;
+    GRID_TOGGLE.style.color = gridVisible ? ACCENT_COLOR : INACTIVE_COLOR;
+
+    removeGridCells();
+    createGridCells();
+}
+
 
 /**
  * Function to create the grid cells.
  */
-function createGridCells(rowsAndCols) {
+function createGridCells() {
+    // Equal width and height of the pixel (square box in the grid).
+    let widthOrHeight = 0;
+
     // Total number of pixels in the grid (e.g. 16x16 = 256).
     for(let i = 0; i < (rowsAndCols * rowsAndCols); i++) {
         const GRID_CELL = document.createElement("div");
 
-        GRID_CELL.style.width = GRID_CELL.style.height = `${(GRIDSIZE / rowsAndCols)}px`;
-        GRID_CELL.classList.add("cell");
+        if(gridVisible) {
+            widthOrHeight = `${(parseInt(GRID_WIDTH) / rowsAndCols)}px`;
+            GRID_CELL.style.border = "1px solid whitesmoke";
+        } else if(!gridVisible) {
+            widthOrHeight = `${(parseInt(GRID_WIDTH) / rowsAndCols)}px`;
+            GRID_CELL.style.border = "none";
+        }
+
+        GRID_CELL.style.width = GRID_CELL.style.height = widthOrHeight;
 
         SKETCH_AREA.appendChild(GRID_CELL);
 
         GRID_CELL.addEventListener("mouseenter", setBackgroundColor);
     }
 }
+
+
 
 function setBackgroundColor() {
     this.style.backgroundColor = "black";
@@ -46,11 +68,12 @@ function removeGridCells() {
 }
 
 SLIDER.oninput = function () {
-    let text = `${this.value} x ${this.value} (Resolution)`;
-    SLIDER_VALUE.textContent = text;
+    rowsAndCols = this.value;
+    SLIDER_VALUE.textContent = `${this.value} x ${this.value} (Resolution)`;
     removeGridCells();
-    createGridCells(this.value);
+    createGridCells();
 }
 
+GRID_TOGGLE.addEventListener("click", toggleGrid); //Toggles the grid on click event.
 
-createGridCells(32);
+createGridCells();
