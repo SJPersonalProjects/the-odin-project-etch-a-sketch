b8c9313b7a46502f536c17fb44adb99f1fa5afc4
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2024-09-12 12:21:39 +0500

    Implement slider for grid resolution settings

--- scripts/script.js
+++ scripts/script.js
@@ -6,13 +6,18 @@ let rowsAndCols = 16;
 
 
 const SKETCH_AREA = document.querySelector("#sketch-area");
+const SLIDER_CONTAINER = document.querySelector("#container");
+const SLIDER = document.querySelector("#slider");
+const SLIDER_VALUE = document.querySelector("#slider-value");
+
+SLIDER_VALUE.textContent = `${SLIDER.value} x ${SLIDER.value} (Resolution)`;
 // Chained assignment.
 SKETCH_AREA.style.width = SKETCH_AREA.style.height = `${GRIDSIZE}px`;
 
 /**
  * Function to create the grid cells.
  */
-function createGridCells() {
+function createGridCells(rowsAndCols) {
     // Total number of pixels in the grid (e.g. 16x16 = 256).
     for(let i = 0; i < (rowsAndCols * rowsAndCols); i++) {
         const GRID_CELL = document.createElement("div");
@@ -30,5 +35,22 @@ function setBackgroundColor() {
     this.style.backgroundColor = "black";
 }
 
+// Removes all the children (pixels or cells) of the sketch area.
+function removeGridCells() {
+    // Every time the first child gets removed
+    // the next child (pixel or cell) becomes the first child.
+    while(SKETCH_AREA.firstChild) {
+        // Hence every single cell gets removed.
+        SKETCH_AREA.firstChild.remove();
+    }
+}
 
-createGridCells();
+SLIDER.oninput = function () {
+    let text = `${this.value} x ${this.value} (Resolution)`;
+    SLIDER_VALUE.textContent = text;
+    removeGridCells();
+    createGridCells(this.value);
+}
+
+
+createGridCells(32);
